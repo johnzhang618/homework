d215867333e6105184614c321537ba39509b96f5
yuan zhang <yuan.zhang2@yuans-MacBook-Pro.local>
2022-01-07 09:53:29 +0800

    fix eslint error no-param-reassign

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -20,17 +20,30 @@ export const formatTimeLabel = (timestamp) => {
 };
 
 export const groupByDay = (readings) => {
-  const groupedByDay = readings.reduce((curr, { time, value }) => {
+  // eslint-disable-next-line prefer-const
+  let groupedByDay = [];
+  readings.forEach(({ time, value }) => {
     const readingDate = new Date(time);
     const day = new Date(
       readingDate.getFullYear(),
       readingDate.getMonth(),
       readingDate.getDate(),
     ).getTime();
-    if (!curr[day]) curr[day] = 0;
-    curr[day] += value;
-    return curr;
-  }, {});
+    if (!groupedByDay[day]) groupedByDay[day] = 0;
+    groupedByDay[day] += value;
+  });
+  // TODO: ⬇ has error with Assignment to property of function parameter
+  // const groupedByDay = readings.reduce((curr, { time, value }) => {
+  //   const readingDate = new Date(time);
+  //   const day = new Date(
+  //     readingDate.getFullYear(),
+  //     readingDate.getMonth(),
+  //     readingDate.getDate(),
+  //   ).getTime();
+  //   if (!curr[day]) curr[day] = 0;
+  //   curr[day] += value;
+  //   return curr;
+  // }, {});
   return Object.entries(groupedByDay).map(([day, value]) => ({
     time: Number(day),
     value,
