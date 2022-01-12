49d73f0d0b539b49329db37839719fea8c9c4416
yuan zhang <yuan.zhang2@yuans-MacBook-Pro.local>
2022-01-12 12:12:28 +0800

    fix warning unknow word

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/css/bass.css';
 import './assets/css/index.css';
-import './assets/css/basscss.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
