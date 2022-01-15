707d2b9b82a7a2f5c95e3da26fd74d7c6651b6d1
yuan zhang <yuan.zhang2@yuans-MacBook-Pro.local>
2022-01-15 16:39:28 +0800

    delete redundancy

--- src/components/molecules/index.jsx
+++ src/components/molecules/index.jsx
@@ -1,11 +1,9 @@
-import DateInputs from './DateInputs/DateInputs';
 import DeviceCard from './DeviceCard/DeviceCard';
 import OverviewCard from './OverviewCard/OverviewCard';
 import UsageBarChart from './UsageBarChart/UsageBarChart';
 import DateSelectors from './DateSelectors/DateSelectors';
 
 export {
-  DateInputs,
   DeviceCard,
   OverviewCard,
   UsageBarChart,
